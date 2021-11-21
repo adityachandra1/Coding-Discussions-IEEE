b2f86b31b3a352579c3b3d45af8d406c828e3b56
Aditya Chandra <adityachandra9898@gmail.com>
2021-11-21 13:47:50 +0530

    19-11-2021

--- Google (19-11-2021)/hotel_rooms.cpp
+++ Google (19-11-2021)/hotel_rooms.cpp
@@ -3,20 +3,25 @@
 //basic snip
 using namespace std;
 
-string mostBooked(vector<string> room_queries){
-   map <string, int> booking_count;
-   int total_room_queries = room_queries.size();
-   for(int i = 0 ; i < total_room_queries ; i++)
-       if(room_queries[i][0] == '+') booking_count[room_queries[i]]++;
+string mostBooked(vector<string> room_queries) {
+    map<string, int> booking_count;
+    int total_room_queries = room_queries.size();
+    
+    for (int i = 0; i < total_room_queries; i++) {
+        if (room_queries[i][0] == '+') {
+            booking_count[room_queries[i]]++;
+        }
+    }
 
     int maxBooking = INT_MIN;
     string mostBookedRoom;
-    for(auto room : booking_count){
-        if(room.second > maxBooking){
+    for (auto room : booking_count) {
+        if (room.second > maxBooking) {
             mostBookedRoom = room.first.substr(1);
             maxBooking = room.second;
         }
     }
+
     return mostBookedRoom;
 }
 
@@ -24,8 +29,10 @@ int main() {
     int total_room_queries;
     cin >> total_room_queries;
     vector<string> room_queries(total_room_queries);
-    for(int i = 0 ; i < total_room_queries ; i++){
+
+    for (int i = 0; i < total_room_queries; i++) {
         cin >> room_queries[i];
     }
+
     cout << mostBooked(room_queries) << endl;
 }
